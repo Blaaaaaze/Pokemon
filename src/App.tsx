be45238fb3dcf288f5deed0ce3d217da44f02b7f
Igor <igorsmirnov604@gmail.com>
2026-08-05 22:07:13 +0300

    feat: сделана верстка карточки

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css'
+import Card from './components/Card/Card'
 import Header from './components/Header/Header'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Card />
+      <Card />
+    </>
   )
 }
 
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,20 @@
+import styles from './Card.module.scss';
+import charmander from '../../assets/imgs/Charmander.jpg';
+
 const Card = () => {
     return (
-        
+        <div className={styles.card}>
+            <div className={styles.card__image}>
+                <img src={charmander} alt="pokename" />
+            </div>
+            <div className={styles.card__content}>
+                <h3 className={styles.card__title}>Pokename</h3>
+                <div className={styles.card__icons}>
+
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
