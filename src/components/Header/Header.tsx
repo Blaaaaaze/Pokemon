e569e27eb3e21d086a6d5a42fdae255e72e0f514
Igor <igorsmirnov604@gmail.com>
2026-08-26 19:17:44 +0300

    feat: нажатие на логотип стирает все параметры

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,27 @@
+import { setSearch, setType } from '../../features/Controls/controls-slice';
+import { setCurrentPage } from '../../features/PokemonList/pokemons-slice';
+import { useAppDispatch } from '../../store';
 import styles from './Header.module.scss';
 import { Link } from 'react-router';
 
-const Header = () => (
-    <header className={styles.header}>
-        <Link to={'/'}>
-            <h1 className={styles.header__title}>
+const Header = () => {
+    const dispatch = useAppDispatch();
+
+    return (
+        <header className={styles.header}>
+            <Link to="/"
+                onClick={() => {
+                    dispatch(setSearch(''));
+                    dispatch(setType(''));
+                    dispatch(setCurrentPage(1));
+                }}>
+                <h1 className={styles.header__title}>
                     Pokemon
-            </h1>
-        </Link>
-    </header>
-);
+                </h1>
+            </Link>
+        </header>
+    );
+};
 
 
 export default Header;
